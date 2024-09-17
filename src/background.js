a258e12a157a38dc97e0646dec4e20249f388176
Thibault Neveu <thibaultneveu@Thibaults-MBP.lan>
2024-09-17 08:40:24 +0200

    Fixe init ext

--- src/background.js
+++ src/background.js
@@ -1,7 +1,9 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.request === 'getNotionUrl') {
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+          console.log('tabs:', tabs);
           var notionUrl = tabs[0].url;
+          console.log('Notion URL:', notionUrl);
           sendResponse({ notionUrl });
       });
       return true; // Keep the message channel open for sendResponse
@@ -14,13 +16,17 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
   if (!tab.url) return;
   const url = new URL(tab.url);
   // Enables the side panel on google.com
+  console.log('url.origin:', url.origin);
+  console.log('NOTION_ORIGIN:', NOTION_ORIGIN);
   if (url.origin === NOTION_ORIGIN) {
+    console.log('enabling side panel');
     await chrome.sidePanel.setOptions({
       tabId,
       path: 'popup.html',
       enabled: true
     });
   } else {
+    console.log('disabling side panel');
     await chrome.sidePanel.setOptions({
       tabId,
       enabled: false
